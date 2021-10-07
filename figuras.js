c617bca9ba12cf21616ce2cf31e9628ecdf2c0c7
Rockdimla <aaje.ms.a@gmail.com>
2021-10-06 19:57:32 -0500

    agregué codigo para calcular area y perimetro de triangulos

--- figuras.js
+++ figuras.js
@@ -22,12 +22,12 @@ function areaRectangulo(lado, altura){
 
 // Funciones para el triangulo.
 
-function perimetroTriangulo(lado1, lado2, lado3){
-	return lado1 + lado2 + lado3;
+function perimetroTriangulo(lado_a, lado_b, lado_c){
+	return (lado_a + lado_b + lado_c);
 }
 
 function areaTriangulo(base, altura){
-	return base * altura;
+	return (base * altura) / 2;
 }
 
 // Funciones para el circulo.
@@ -47,14 +47,14 @@ function areaCirculo(radio){
 
 function calcularPerimetroCuadrado(){
 	const input = document.getElementById("lado-cuadrado");
-	const value = input.value;
+	const value = parseFloat(input.value);
 	const perimetro = perimetroCuadrado(value);
 	alert("El perímetro de este cuadrado es: " + perimetro + "cm");
 }
 
 function calcularAreaCuadrado(){
 	const input = document.getElementById("lado-cuadrado");
-	const value = input.value;
+	const value = parseFloat(input.value);
 	const area = areaCuadrado(value);
 	alert("El área de este cuadrado es: " + area + "cm²");
 }
@@ -62,8 +62,8 @@ function calcularAreaCuadrado(){
 function calcularPerimetroRectangulo(){
 	const lado = document.getElementById("lado-rectangulo");
 	const ancho = document.getElementById("ancho-rectangulo");
-	const value1 = lado.value;
-	const value2 = ancho.value;
+	const value1 = parseFloat(lado.value);
+	const value2 = parseFloat(ancho.value);
 	const perimetro = perimetroRectangulo(value1, value2);
 	alert("El perímetro de este rectangulo es: " + perimetro + "cm");
 }
@@ -71,8 +71,75 @@ function calcularPerimetroRectangulo(){
 function calcularAreaRectangulo(){
 	const lado = document.getElementById("lado-rectangulo");
 	const ancho = document.getElementById("ancho-rectangulo");
-	const value1 = lado.value;
-	const value2 = ancho.value;
+	const value1 = parseFloat(lado.value);
+	const value2 = parseFloat(ancho.value);
 	const area = areaRectangulo(value1, value2);
 	alert("El área de este rectangulo es: " + area + "cm²");
 }
+
+function calcularPerimetroTriangulo(){
+	const a = document.getElementById("lado-triangulo-a");
+	const b = document.getElementById("lado-triangulo-b");
+	const c = document.getElementById("lado-triangulo-c");
+
+	const lado_a = parseFloat(a.value);
+	const lado_b = parseFloat(b.value);
+	const lado_c = parseFloat(c.value);
+
+	
+
+	var e = document.getElementById("tipo-triangulo");
+	switch(e.selectedIndex){
+		case 0: //triangulo equilatero
+			var perimetro = perimetroTriangulo(lado_a, lado_a, lado_a);
+			break;
+		case 1: //triangulo isosceles
+			var perimetro = perimetroTriangulo(lado_a, lado_b, lado_b);
+			break;
+		case 2: //triangulo escaleno
+			var perimetro = perimetroTriangulo(lado_a, lado_b, lado_c);
+			break;
+	}
+
+	alert("El perímetro de este rectangulo es: " + perimetro + "cm");
+}
+
+function calcularAreaTriangulo(){
+	const b = document.getElementById("base-triangulo");
+	const a = document.getElementById("alto-triangulo");
+	const base = parseFloat(b.value);
+	const alto = parseFloat(a.value);
+	const area = areaTriangulo(base, alto);
+	alert("El perímetro de este rectangulo es: " + area + "cm²");
+}
+
+//MOSTRAR INPUT CUANDO CAMBIA EL TIPO DE TRIANGULO XD
+
+const selectElement = document.querySelector('.tipo-triangulo');
+
+selectElement.addEventListener('change', (event) => {
+
+    var e = document.getElementById("tipo-triangulo");
+	var lado_a = document.getElementById('lado-triangulo-a');
+	var lado_b = document.getElementById('lado-triangulo-b');
+	var lado_c = document.getElementById('lado-triangulo-c');
+
+	switch (e.selectedIndex){
+		case 0:
+			lado_a.style.visibility = "visible";
+			lado_b.style.visibility = "hidden";
+			lado_c.style.visibility = "hidden";
+		break;
+		case 1:
+			lado_a.style.visibility = "visible";
+			lado_b.style.visibility = "visible";
+			lado_c.style.visibility = "hidden";
+		break;
+		case 2:
+			lado_a.style.visibility = "visible";
+			lado_b.style.visibility = "visible";
+			lado_c.style.visibility = "visible";
+		break;
+	}
+	
+});
